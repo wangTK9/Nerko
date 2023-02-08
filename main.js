8498d04009cbf48821e8183aabdd443ac321df61
wangTK9 <nguyentuankiet6693@gmail.com>
2023-02-08 23:25:45 +0700

    up load

---
+++ main.js
@@ -0,0 +1,14 @@
+var leftBtn = document.querySelector('#left')
+var rightBtn = document.querySelector('#right')
+var road = document.querySelectorAll('.road__map')
+
+
+
+
+        
+
+
+
+leftBtn.addEventListener('click',clickBtn)
+rightBtn.addEventListener('click',clickBtn)
+
